b27a5d7886f83bacddcb7dee8c54af3f74166caf
JamesMwangi <jamohgamer@gmail.com>
2024-04-12 09:01:01 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+const bodi = document.querySelector("#bodie");
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then(responce => responce.json())
+    .then(user => {
+      bodi.innerHTML = user.id;
+    })
+    .catch(error => {
+      bodi.innerHTML = error.message;
+    });
+}
